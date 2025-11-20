6057584db8c4ab4af0d28e7d8ee5a22449362156
voiteshonok <vsvoiteshonok1@gmail.com>
2025-11-20 22:15:38 +0200

    refactor

--- pr-1/task/index.js
+++ pr-1/task/index.js
@@ -12,25 +12,21 @@ const isQuiet = args.includes("--quiet");
 
 const logger = getLogger(isVerbose, isQuiet);
 
+const studentsStorage = new StudentsStorage();
+logger.log(studentsStorage.getAllStudents());
+studentsStorage.addStudent('new', 23, 'new');
+
+logger.log(studentsStorage.getStudentsByGroup(2));
+
+saveToJSON(studentsStorage.getAllStudents(), './students.json');
+
+logger.log(studentsStorage.calculateAverageAge());
+logger.log(studentsStorage.getStudentById('5'));
+
+const newStudents = loadJSON('./students.json');
+logger.log(newStudents);
+
+studentsStorage.removeStudent('4');
+logger.log(studentsStorage.getAllStudents());
 
 
-(() => {
-  const studentsStorage = new StudentsStorage();
-  logger.log(studentsStorage.getAllStudents());
-  studentsStorage.addStudent('new', 23, 'new');
-
-  logger.log(studentsStorage.getStudentsByGroup(2));
-
-  saveToJSON(studentsStorage.getAllStudents(), './students.json');
-
-  logger.log(studentsStorage.calculateAverageAge());
-  logger.log(studentsStorage.getStudentById('5'));
-
-  const newStudents = loadJSON('./students.json');
-  logger.log(newStudents);
-
-  studentsStorage.removeStudent('4');
-  logger.log(studentsStorage.getAllStudents());
-
-
-})();
