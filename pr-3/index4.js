650a947b900a56ce983db2fea313efc3ffcdc4eb
voiteshonok <vsvoiteshonok1@gmail.com>
2025-12-07 21:29:25 +0200

    lab-3 minimal frontend-backend

--- pr-3/index4.js
+++ pr-3/index4.js
@@ -14,6 +14,18 @@ const backup = new Backup();
 const express = require('express')
 const app = express()
 app.use(express.json())
+
+// Enable CORS for frontend
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 const port = 3000
 
 app.get('/api/students', (req, res) => {
